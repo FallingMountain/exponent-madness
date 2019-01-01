b7043b2589a9450290197a705aa2b53573f5b27a
math123468 <44072698+math123468@users.noreply.github.com>
2019-01-01 09:04:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -344,7 +344,7 @@ function abbreviate2(i,short) {
 	}
 	var returning = ''
 	var units = ["","U","D","T","Qa","Qi","Sx","Sp","O","N"]; // prefixes for ones place
-	var tens = ["","Dc","Vg","Tg","Qag","Qig","Sxg","Spg","Og","Ng"]; // prefixes for tens place
+	var tens = ["","Dc","Vg","Tg","Qag","Qig","Sxg","Spg","Og","Nn"]; // prefixes for tens place
 	var hundreds = ["","Cn","Dcn","Tcn","Qac","Qic","Sxc","Spx","Ocn","Nc"]
 	var thousands = ['','Mi-','Mc-','Nn-']
 	var i2=Math.floor(i/10);
